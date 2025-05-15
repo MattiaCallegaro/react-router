5bdcf2fbe161e5ad558ddff62a94f69d5094c335
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-15 15:22:29 +0200

    [UPD] Add pagses & BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
+import HomePage from './pages/HomePage'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
 
 function App() {
-
+  //mi creo la variabile di stato  ( stato=currentPage | aggiorna valore=setCurrentPage | "home"=valore iniziale di stato)
+  const [currentPage, setCurrentPage] = useState("home")
 
   return (
     <>

06130209bc5024e665d99e40a48aa3430dc93355
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-15 15:37:17 +0200

    [UPD] Add Navlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import About from './pages/About'
@@ -7,10 +7,17 @@ import Contacts from './pages/Contacts'
 
 function App() {
   //mi creo la variabile di stato  ( stato=currentPage | aggiorna valore=setCurrentPage | "home"=valore iniziale di stato)
-  const [currentPage, setCurrentPage] = useState("home")
+  // const [currentPage, setCurrentPage] = useState("home")
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={HomePage}></Route>
+          <Route path='/about' Component={About}></Route>
+          <Route path='/contacts' Component={Contacts}></Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )

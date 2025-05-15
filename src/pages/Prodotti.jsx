92fb0a73b01772b33c86a0c8e2c7d5ba6675bd33
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-15 16:58:23 +0200

    [UPD] Add card products

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,40 +1,50 @@
-import React, { useEffect, useState } from 'react'
+
 import MainNavbar from '../components/MainNavbar'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const Prodotti = () => {
+//mi faccio la chiamata ajax direttamente all'apertuta della pagina
+useEffect(() => {
+    axios.get('https://fakestoreapi.com/products').then((resp) => {
+        setProducts(resp.data)
+    })
 
-    const [prodotti, setProdotti] = useState([])
-
-    const getProdotti = () => {
-        const axios = require('axios');
-        axios.get('https://fakestoreapi.com/products').then((resp) => {
-            setProdotti(resp.data)
-        })
-    }
-
-    useEffect(() => {
-        getProdotti()
-    }, [])
+}, [])
 
 
-    return (
-        <>
-            < MainNavbar />
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h1>Lista prodotti</h1>
-                    </div>
-                </div>
-                <div className="row">
-
+return (
+    <>
+        <MainNavbar />
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1>Lista prodotti</h1>
                 </div>
             </div>
+            <div className="row">
+                {products.map((product) => {
+                    return (
+                        <div className="col-12 col-md-6">
+                            <div className="card">
+                                <div className="card-image-top">
+                                    <img src={product.image} alt="" />
+                                </div>
+                                <div className="card-body">
+                                    <h3>{product.title}</h3>
+                                    <p>{product.category}</p>
+                                    <p>{product.description}</p>
+                                    <p>{product.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
 
-        </>
-    )
+            </div>
+        </div>
+
+    </>
+)
 }
 
 export default Prodotti

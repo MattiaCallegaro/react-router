e984cabe6491e4304d18b2602046fa969178407d
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-16 15:36:07 +0200

    [UPD] Add new Route=DefaultLayout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Prodotti from './pages/Prodotti'
 import ChiSiamo from './pages/ChiSiamo'
+//importo layout per utilizzare outlet
+import DefaultLayout from './layouts/DefaultLayout'
 
 function App() {
   //mi creo la variabile di stato  ( stato=currentPage | aggiorna valore=setCurrentPage | "home"=valore iniziale di stato)
@@ -14,9 +16,11 @@ function App() {
       {/* `Definisco le rotte */}
       <BrowserRouter>
         <Routes>
-          <Route path='/' Component={HomePage}></Route>
-          <Route path='/Chi-siamo' Component={ChiSiamo}></Route>
-          <Route path='/Prodotti' Component={Prodotti}></Route>
+          <Route Component={DefaultLayout}>
+            <Route path='/' Component={HomePage}></Route>
+            <Route path='/Chi-siamo' Component={ChiSiamo}></Route>
+            <Route path='/Prodotti' Component={Prodotti}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
 
